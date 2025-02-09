9f9afb68fd5ec92baaa170ec09d390f114264de4
Raghu Gannaram <raghuvarangannaram@gmail.com>
2025-02-09 19:45:12 +0530

    updated the yummy_yarns references

--- cinnamon/urls.py
+++ cinnamon/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for yummy_yarns project.
+URL configuration for cinnamon project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
